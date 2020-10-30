1f0f7a869df9b47867641406662d649c2836ea9c
jiangwei.chen <jiangwei.chen@tigerobo.com>
2020-10-30 15:13:07 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <template>
-  {{home}}
+  <svg version="1.1"
+     baseProfile="full"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+
+    <rect width="100%" height="100%" fill="red" />
+    <circle cx="150" cy="100" r="80" fill="green" />
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">svg</text>
+  </svg>
 </template>
 
 <script lang="ts">
-
 export default {
   name: 'Home',
   data() {
     return {
-      home: '我是home'
+      home: 'home',
+      svg
     }
   }
 }
